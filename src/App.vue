ae93828cc84faf9f28b3cbe821da7f8a7149cb81
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-10-09 10:30:32 +0700

    Create component Login, register and secure page

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,38 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// export default {
+//   name: 'app',
+//   components: {
+//     HelloWorld
+//   }
+// }
+
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn
+    }
+  },
+  methods: {
+    logout: function() {
+      this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login')
+        })
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 Vue.config.productionTip = false
 
